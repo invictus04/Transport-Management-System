7fd8c35301027ed8f5bfa586e0ab550f1c6a8aee
Rajvardhan Singh <vardhanrajsingh04@gmail.com>
2025-12-05 00:07:45 +0530

    intial setup

---
+++ src/main/java/com/TMS/Transport/Management/System/TransportManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.TMS.Transport.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportManagementSystemApplication.class, args);
+	}
+
+}
